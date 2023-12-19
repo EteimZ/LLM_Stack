897dbff0a69e2a73fd421b898fd08ef1410e8924
Eteims <8teims@gmail.com>
2023-12-19 17:26:33 +0100

    fix

--- apps/weaviate-file-q-and-a/client.py
+++ apps/weaviate-file-q-and-a/client.py
@@ -1,9 +1,10 @@
 import weaviate
+import os
 
 # Connect to the weaviate database
 client = weaviate.Client(
     url = "http://localhost:8080",
     additional_headers={
-        "X-OpenAI-Api-Key": "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",
+        "X-OpenAI-Api-Key": os.environ["OPENAI_API_KEY"],
     }
 )
